32e07867f17634a8da40cd916eba71f5867a3efd
James Clement <jameseclement@gmail.com>
2019-01-13 19:58:25 -0500

    first commit

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, color, gender, temperament, alive)
+VALUES ("Mr. Chocolate", 10, "Black", "Male", "Nice", true);
+
+INSERT INTO bears (name, age, color, gender, temperament, alive)
+VALUES ("Rowdy", 3, "Brown", "Male", "Mean", false);
+
+INSERT INTO bears (name, age, color, gender, temperament, alive)
+VALUES ("Tabitha", 19, "White", "Female", "Wise", true);
+
+INSERT INTO bears (name, age, color, gender, temperament, alive)
+VALUES ("Sergeant Brown", 12, "White", "Male", "Strict", true);
+
+INSERT INTO bears (name, age, color, gender, temperament, alive)
+VALUES ("Melissa", 3, "Gray", "Female", "Calm", true);
+
+INSERT INTO bears (name, age, color, gender, temperament, alive)
+VALUES ("Grinch", 21, "Green", "Male", "Angry", false);
+
+INSERT INTO bears (name, age, color, gender, temperament, alive)
+VALUES ("Wendy", 1, "Yellow", "Female", "Happy", true);
+
+INSERT INTO bears (name, age, color, gender, temperament, alive)
+VALUES (null, 3, "Black", "Male", "Very Mean", true);
